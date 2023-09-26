29db09d8f8ca848442a8c90fbbd3489bea3b6858
Adberto Melo <adbertomelo@gmail.com>
2023-09-26 01:29:59 +0000

    ajustes para compilação 3

--- pages/components/grupo.tsx
+++ pages/components/grupo.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { formatDecimal } from '../util/format';
+import FormatDecimal from '../util/FormatDecimal';
 
 interface GrupoProps {
   id: string;
@@ -14,26 +14,26 @@ const Grupo: FC<GrupoProps> = ({ identacao, id, nome, valor, topo }: GrupoProps)
   const classNameGrupo = topo === 'S' ? 'topo-do-grupo' : 'subgrupo';
 
   return (
-        <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-2 gap-4">
 
 
-        <div className="w-1/2">
-          <div className="text-lg font-semibold text-left">
-			<pre>
-              {t.repeat(identacao)}
-              {nome}({id})
-            </pre>
+      <div className="w-1/2">
+        <div className="text-lg font-semibold text-left">
+          <pre>
+            {t.repeat(identacao)}
+            {nome}({id})
+          </pre>
 
-          </div>
         </div>
-        <div className="w-1/2">
-          <div className="text-gray-600 text-right">
+      </div>
+      <div className="w-1/2">
+        <div className="text-gray-600 text-right">
 
-				{formatDecimal(valor)}
+          {FormatDecimal(valor)}
 
-          </div>
         </div>
-      
+      </div>
+
 
     </div>
 
--- pages/util/format.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const { format: formatDecimal } = new Intl.NumberFormat('pt-BR', {
-  style: 'decimal',
-  currency: 'BRL',
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
-
