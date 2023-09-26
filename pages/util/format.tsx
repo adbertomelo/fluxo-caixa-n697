a3759a5d37ea15f578e7a8d6a59c9f14dd427753
Adberto Melo <adbertomelo@gmail.com>
2023-09-26 01:16:05 +0000

    ajustes para compilação

--- pages/util/format.tsx
+++ pages/util/format.tsx
@@ -1,10 +1,9 @@
 
 
-const { format: formatDecimal } = new Intl.NumberFormat('pt-BR', {
+export const { format: formatDecimal } = new Intl.NumberFormat('pt-BR', {
   style: 'decimal',
   currency: 'BRL',
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 });
 
-export default formatDecimal;
